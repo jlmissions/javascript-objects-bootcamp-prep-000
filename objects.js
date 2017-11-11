1215ff74d38e5f5989f9734a6171ed81c38d26e0
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-11 21:12:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {name: "song"}
 
 function updatePlaylist(playlist, name, song){
-  playlist[name] = "song";
+  playlist[name] = song;
   return playlist
 }
 function removeFromPlaylist (playlist, name){
